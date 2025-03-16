bee2fe5068aaf5c380332c2f90b684b76db81377
Sham Ashok Machale <120729282+Shammachale0102@users.noreply.github.com>
2025-03-17 00:28:27 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Palindrome {
+    public static void main (String args[]){
+     Scanner scanner = new Scanner(System.in);
+     System.out.print("Enter a number:");
+     int number = scanner.nextInt();
+     
+     if(isPalindrome(number)){
+     System.out.print(number +"is a palindrome");
+
+     }
+     else{
+        System.out.println(number+"is not a palindrome");
+     }
+     scanner.close();
+    }
+    public static boolean isPalindrome(int number){
+        String original = String.valueOf(number);
+        String reversed = new StringBuilder(original).reverse().toString();
+        return original.equals(reversed);
+    }
+}
